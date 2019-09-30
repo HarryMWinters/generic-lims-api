f91113fba61df5c48e9d7efed80e923bf354244c
HarryMWinters <harrymcwinters@gmail.com>
2019-09-30 12:38:53 -0700

    Rename project

--- MongoPythonBackend/MouseHouseModels/models/pcr_reaction.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-class PCR_Reaction(models.Model):
-    """
-    Defines defines reaction for amplifying sample post DNA extraction.
-    """
-    uuid = models.UUIDField(primary_key=True, unique=True,
-                            null=True, auto_created=True)
