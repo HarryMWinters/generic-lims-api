2f75642e0e544b3c73f4196c3b3080d3a4f1ae13
HarryMWinters <harrymcwinters@gmail.com>
2019-10-12 16:32:14 -0700

    Add better model importing

--- MouseHouseLIMS/backend/models.py
+++ MouseHouseLIMS/backend/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-from .models import *
+from .custom_models import (
+    researcher,
+    mouse,
+    dna_extraction,
+    electrophoresis,
+    gel_image,
+    master_mix,
+    pcr_reaction,
+    protocol,
+    tissue_sample
+)
