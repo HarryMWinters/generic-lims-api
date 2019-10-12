a55e1035ba243672dc55cef4964b8d80970a75c0
HarryMWinters <harrymcwinters@gmail.com>
2019-10-12 16:31:15 -0700

    Fix custom user import

--- MouseHouseLIMS/backend/admin.py
+++ MouseHouseLIMS/backend/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from .custom_models.researcher import Researcher
 
 # Register your models here.
+
+admin.site.register(Researcher, UserAdmin)
